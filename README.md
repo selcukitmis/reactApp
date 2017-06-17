f233a9adaeed5cb8110141624c44745192bdf303
Selçuk İTMİŞ <slck87@hotmail.com>
2017-06-18 00:50:39 +0300

    travis ve codecov eklendi

---
+++ README.md
@@ -0,0 +1,8 @@
+Test için: yarn test
+Testi update için: yarn test -- -u
+Coverage için: CI=true yarn run coverage
+
+
+
+git add.
+git commit -m "açıklama"
--- src/header.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Test extends Component {
-  render() {
-    return <b>asd</b>;
-  }
-}
-
-export default Test;
